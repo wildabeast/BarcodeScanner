10d8da1485ef8fb15cafdfae160c241fcd12a493
Fábio Akira Yoshida <fabioseno@gmail.com>
2014-06-03 15:10:44 -0300

    Scanning not working on tablet.

--- src/windows8/CameraHandler.js
+++ src/windows8/CameraHandler.js
@@ -1,14 +1,14 @@
-/*global exports, Windows, ZXing, require, console */
+cordova.define("com.phonegap.plugins.barcodescanner.CameraHandler", function (require, exports, module) { /*global exports, Windows, ZXing, require, console */
 
-/**
- * cordova is available under *either* the terms of the modified BSD license *or* the
- * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
- *
- * Copyright (c) Vinicius Linck 2014
- * Copyright (c) 2014, Tlantic
- */
+    /**
+     * cordova is available under *either* the terms of the modified BSD license *or* the
+     * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
+     *
+     * Copyright (c) Vinicius Linck 2014
+     * Copyright (c) 2014, Tlantic
+     */
 
-// Using
+    // Using
     var Capture = Windows.Media.Capture,
         previewPanel,
         reader,
@@ -17,18 +17,18 @@
         memoryBuffer,
         hook;
 
-
-
     // decoding barcode
     function readCode(decoder, pixels, format) {
         'use strict';
         var result = reader.decode(pixels, decoder.pixelWidth, decoder.pixelHeight, format);
-        
+
         if (result) {
             dispose();
             hook(result);
         } else {
-            window.requestAnimationFrame(render);
+            window.setTimeout(function () {
+                window.requestAnimationFrame(render);
+            }, 1500);
         }
     }
 
@@ -97,10 +97,13 @@
     }
 
     // gets current preview image frame
-   function flick() {
-    'use strict';
-       var photoProperties = Windows.Media.MediaProperties.ImageEncodingProperties.createJpeg();
-            memoryBuffer = new Windows.Storage.Streams.InMemoryRandomAccessStream();
+    function flick() {
+        'use strict';
+        var photoProperties = Windows.Media.MediaProperties.ImageEncodingProperties.createJpeg();
+
+        memoryBuffer = null;
+
+        memoryBuffer = new Windows.Storage.Streams.InMemoryRandomAccessStream();
 
         sampler.capturePhotoToStreamAsync(photoProperties, memoryBuffer).done(loadStream);
     }
@@ -108,6 +111,7 @@
     // triggers camera image gathering process
     function render() {
         'use strict';
+
         if (!stopped) {
             flick();
         }
@@ -116,15 +120,21 @@
     // dispose camera element
     function dispose() {
         'use strict';
+
         previewPanel.style.display = 'none';
         previewPanel.pause();
-        previewPanel.src = '';
-        stopped = true;
-    }
+        previewPanel.src = null;
 
+        stopped = true;
+
+        if (sampler) {
+            sampler = null;
+        }
+    }
 
     exports.stop = function () {
         'use strict';
+
         dispose();
         hook({ cancelled: true });
     };
@@ -132,45 +142,65 @@
     exports.start = function (callback) {
         'use strict';
         hook = callback;
-        previewPanel.style.display = 'block';
-    previewPanel.src = URL.createObjectURL(sampler);
-    previewPanel.play();
-    stopped = false;
-    render();
-};
- 
-exports.createCameraElm = function () {
-	'use strict';
 
-	console.log('Creating camera element...');
+        dispose();
 
-	var elm = document.createElement('video');
-	
-	elm.style.display = 'none';
-	elm.style.position = 'absolute';
-	elm.style.left = '0px';
-	elm.style.top = '0px';
-	elm.style.zIndex = 2e9;
-	elm.style.width = '100%';
-	elm.style.height = '100%';
+        sampler = new Windows.Media.Capture.MediaCapture();
+        sampler.initializeAsync().done(function (result) {
+            stopped = false;
 
-	elm.onclick = this.stop;
+            previewPanel.style.display = 'block';
+            previewPanel.src = URL.createObjectURL(sampler);
+            previewPanel.play();
 
-	return elm;
-};
+            window.setTimeout(function () {
+                render();
+            }, 500);
 
-exports.initCamera = function () {
-	'use strict';
-	console.log('Configuring image sampler...');
+        });
+    };
 
-	// preparing camera preview
-	reader = new ZXing.BarcodeReader();
-	sampler = new Capture.MediaCapture();
-	sampler.initializeAsync();
+    var videoTagAppended = false;
 
-	// adding preview element into DOM tree
-	previewPanel = this.createCameraElm();
-	document.body.appendChild(previewPanel);
-};
+    exports.createCameraElm = function () {
+        'use strict';
 
- require('cordova/windows8/commandProxy').add('CameraHandler', exports);
+        console.log('Creating camera element...');
+
+        var elm = document.createElement('video');
+
+        elm.style.display = 'none';
+        elm.style.position = 'absolute';
+        elm.style.left = '0px';
+        elm.style.top = '0px';
+        elm.style.zIndex = 2e9;
+        elm.style.width = '100%';
+        elm.style.height = '100%';
+
+        elm.onclick = this.stop;
+
+        return elm;
+    };
+
+    exports.initCamera = function () {
+        'use strict';
+        console.log('Configuring image sampler...');
+
+        // preparing camera preview
+        reader = new ZXing.BarcodeReader();
+        sampler = new Windows.Media.Capture.MediaCapture();
+        sampler.initializeAsync();
+
+        // adding preview element into DOM tree
+        if (!previewPanel) {
+            previewPanel = this.createCameraElm();
+        }
+
+        if (document.body && !videoTagAppended) {
+            document.body.appendChild(previewPanel);
+            videoTagAppended = true;
+        }
+    };
+
+    require('cordova/windows8/commandProxy').add('CameraHandler', exports);
+});
