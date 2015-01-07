3a05ca24e2aeb4ba2559834dc8b84223df6a498e
joskarthic <joskarthick3@gmail.com>
2015-01-07 14:08:25 +0530

    Update CameraManager.java
    I need camera Windows bigger

--- src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java
+++ src/android/LibraryProject/src/com/google/zxing/client/android/camera/CameraManager.java
@@ -39,10 +39,10 @@ public final class CameraManager {
 
   private static final String TAG = CameraManager.class.getSimpleName();
 
-  private static final int MIN_FRAME_WIDTH = 240;
-  private static final int MIN_FRAME_HEIGHT = 240;
-  private static final int MAX_FRAME_WIDTH = 600;
-  private static final int MAX_FRAME_HEIGHT = 400;
+  private static final int MIN_FRAME_WIDTH = 1240;
+  private static final int MIN_FRAME_HEIGHT = 1240;
+  private static final int MAX_FRAME_WIDTH = 1600;
+  private static final int MAX_FRAME_HEIGHT = 1400;
 
   private final Context context;
   private final CameraConfigurationManager configManager;
